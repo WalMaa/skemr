a90f31220492e44b489cd5d7db74c00dc9c5f212
WalMaa <98175031+WalMaa@users.noreply.github.com>
2025-08-01 23:58:10 +0300

    started implementing concurrency in rule engine

--- rulengn/rule_engine.go
+++ rulengn/rule_engine.go
@@ -6,6 +6,7 @@ import (
 	"github.com/walmaa/skemr/db/sqlc"
 	"github.com/walmaa/skemr/parser"
 	"log/slog"
+	"sync"
 )
 
 type RuleEngine struct {
@@ -16,8 +17,30 @@ func NewRuleEngine(q sqlc.Querier) *RuleEngine {
 	return &RuleEngine{db: q}
 }
 
+func (r *RuleEngine) ProcessStatements(c context.Context, statements []string, database *sqlc.Database) (chan bool, error) {
+	results := make(chan bool, len(statements))
+	var wg sync.WaitGroup
+	for _, statement := range statements {
+		stmt := statement
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			slog.Info("Processing statement", slog.String("statement", stmt))
+			results <- r.CheckStatement(c, stmt, database)
+		}()
+
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+	return results, nil
+}
+
 // CheckStatement checks if the given SQL statement matches any rules in the database for the specified project.
-func (r *RuleEngine) CheckStatement(c context.Context, statement string, project *sqlc.Project) bool {
+func (r *RuleEngine) CheckStatement(c context.Context, statement string, database *sqlc.Database) bool {
+	slog.Info("CheckStatement", slog.String("statement", statement))
 	stmtact, err := parser.ParseSql(statement)
 
 	if err != nil {
@@ -26,7 +49,7 @@ func (r *RuleEngine) CheckStatement(c context.Context, statement string, project
 	}
 
 	args := sqlc.ListRulesByCriteriaParams{
-		ProjectID:    project.ID,
+		DatabaseID:   database.ID,
 		Scope:        sqlc.RuleScopeTable,
 		RelationName: &stmtact.Relation,
 		Target:       stmtact.Target,
--- test/rule_engine_test.go
+++ test/rule_engine_test.go
@@ -2,12 +2,13 @@ package test
 
 import (
 	"context"
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"skemr/db/sqlc"
-	"skemr/rulengn"
-	"skemr/test/mocks"
+	"github.com/walmaa/skemr/db/sqlc"
+	"github.com/walmaa/skemr/rulengn"
+	"github.com/walmaa/skemr/test/mocks"
 	"testing"
 )
 
@@ -17,22 +18,56 @@ func TestCheckStatement(t *testing.T) {
 	ruleEngine := rulengn.NewRuleEngine(mockDB)
 
 	// Define a sample project
-	project := &sqlc.Project{ID: uuid.New(), Name: "Test Project"}
+	database := &sqlc.Database{ID: uuid.New(), Name: "Test Database"}
 
 	// Define a sample SQL statement
 	statement := "ALTER TABLE users DROP COLUMN age;"
 	relation := "users"
 	// Mock the database response
 	mockDB.On("ListRulesByCriteria", mock.Anything, sqlc.ListRulesByCriteriaParams{
-		ProjectID:    project.ID,
+		DatabaseID:   database.ID,
 		Scope:        sqlc.RuleScopeTable,
 		RelationName: &relation,
 		Target:       "age",
 	}).Return([]sqlc.Rule{}, nil)
 
 	// Call the CheckStatement method
-	result := ruleEngine.CheckStatement(context.Background(), statement, project)
+	result := ruleEngine.CheckStatement(context.Background(), statement, database)
 
 	// Assert the result
 	assert.True(t, result)
+}
+
+func TestProcessStatement(t *testing.T) {
+	// Initialize the rule engine with a mock database
+	mockDB := &mocks.Querier{}
+	ruleEngine := rulengn.NewRuleEngine(mockDB)
+
+	// Define a sample project
+	database := &sqlc.Database{ID: uuid.New(), Name: "Test Database"}
+
+	// Define a sample SQL statement
+	statements := []string{"ALTER TABLE users DROP COLUMN age;",
+		"CREATE TABLE orders (id SERIAL PRIMARY KEY, user_id INT, amount DECIMAL);",
+		"INSERT INTO users (name, email) VALUES ('John Doe', 'johg');"}
+
+	// Mock the database response
+	mockDB.On("ListRulesByCriteria", mock.Anything, mock.Anything).Return([]sqlc.Rule{}, nil)
+
+	// Call the processStatements method
+	results, err := ruleEngine.ProcessStatements(context.Background(), statements, database)
+	fmt.Println(results)
+
+	assert.NoError(t, err)
+
+	// Consume the results to trigger execution and logs
+	var resultCount int
+	for res := range results {
+		assert.True(t, res)
+		resultCount++
+	}
+
+	// Assert we received the expected number of results
+	assert.Equal(t, len(statements), resultCount)
+
 }
